289c9355a802612575b6b1b5055360c3ca746ba1
Keith Hardy <keithjnrhardy@gmail.com>
2025-01-03 22:13:19 +0000

    Installed Prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,26 @@
+// ts-ignore 7017 is used to ignore the error that the global object is not
+// defined in the global scope. This is because the global object is only
+// defined in the global scope in Node.js and not in the browser.
+
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
+
+const prismaClientSingleton = () => {
+  return new PrismaClient().$extends(withAccelerate())
+}
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = global as unknown as { prisma: PrismaClientSingleton }
+
+export const prisma = globalForPrisma.prisma || prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
