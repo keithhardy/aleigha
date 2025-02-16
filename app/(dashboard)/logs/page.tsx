d97ee4fc19e8ca6e242b95401330cdc8b7a027f8
Keith Hardy <keithjnrhardy@gmail.com>
2025-02-16 22:48:58 +0000

    Update

--- app/(dashboard)/logs/page.tsx
+++ app/(dashboard)/logs/page.tsx
@@ -1,15 +1,29 @@
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { prisma } from "@/lib/prisma";
 
 export default async function Users() {
-  const logs = await prisma.log.findMany()
+  const logs = await prisma.log.findMany();
 
   return (
-    <div className="space-y-2">
-      {logs.map((log) => (
-        <div key={log.id} className="flex flex-col space-y-2">
-          <p>{log.message}</p>
-        </div>
-      ))}
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Created At</TableHead>
+            <TableHead>User</TableHead>
+            <TableHead>Message</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {logs.map((log) => (
+            <TableRow key={log.id}>
+              <TableCell className="h-12">{log.createdAt.toLocaleString()}</TableCell>
+              <TableCell>{log.userId}</TableCell>
+              <TableCell>{log.message}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 };
