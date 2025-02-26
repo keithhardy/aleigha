26e7746f1aa1603d8dc54b20e7473eef3b598096
Keith Hardy <keithjnrhardy@gmail.com>
2025-02-26 15:30:15 +0000

    Update

--- app/(dashboard)/logs/page.tsx
+++ app/(dashboard)/logs/page.tsx
@@ -1,38 +1,39 @@
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { prisma } from "@/lib/prisma";
+// import {
+//   Table,
+//   TableBody,
+//   TableCell,
+//   TableHead,
+//   TableHeader,
+//   TableRow,
+// } from "@/components/ui/table";
+// import { prisma } from "@/lib/prisma";
 
 export default async function Users() {
-  const logs = await prisma.log.findMany();
+  // const logs = await prisma.log.findMany();
 
-  return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Created At</TableHead>
-            <TableHead>User</TableHead>
-            <TableHead>Message</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {logs.map((log) => (
-            <TableRow key={log.id}>
-              <TableCell className="h-12">
-                {log.createdAt.toLocaleString()}
-              </TableCell>
-              <TableCell>{log.userId}</TableCell>
-              <TableCell>{log.message}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
-  );
+  return
+  // (
+  // <div className="rounded-md border">
+  //   <Table>
+  //     <TableHeader>
+  //       <TableRow>
+  //         <TableHead>Created At</TableHead>
+  //         <TableHead>User</TableHead>
+  //         <TableHead>Message</TableHead>
+  //       </TableRow>
+  //     </TableHeader>
+  //     <TableBody>
+  //       {logs.map((log) => (
+  //         <TableRow key={log.id}>
+  //           <TableCell className="h-12">
+  //             {log.createdAt.toLocaleString()}
+  //           </TableCell>
+  //           <TableCell>{log.userId}</TableCell>
+  //           <TableCell>{log.message}</TableCell>
+  //         </TableRow>
+  //       ))}
+  //     </TableBody>
+  //   </Table>
+  // </div>
+  // );
 }
