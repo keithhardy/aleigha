5ecf2d8e1e3c13144d61cdfda548b77bfac03fbb
Keith Hardy <keithjnrhardy@gmail.com>
2025-02-26 22:33:23 +0000

    Update

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -15,8 +15,8 @@ import { getCurrentUser } from "@/lib/auth";
 
 import { DashboardBreadcrumb } from "./components/breadcrumb";
 import { DashboardSidebar } from "./components/dashboard-sidebar";
-import { MessageMenu } from "./components/message-menu";
-import { NotificationsMenu } from "./components/notifications-menu";
+// import { MessageMenu } from "./components/message-menu";
+// import { NotificationsMenu } from "./components/notifications-menu";
 
 export default async function DashboardLayout({
   children,
@@ -41,10 +41,10 @@ export default async function DashboardLayout({
             <DashboardBreadcrumb />
           </div>
           <div className="flex items-center gap-2 px-4">
-            <NotificationsMenu />
+            {/* <NotificationsMenu />
             <Separator orientation="vertical" className="h-4" />
             <MessageMenu />
-            <Separator orientation="vertical" className="h-4" />
+            <Separator orientation="vertical" className="h-4" /> */}
             <ThemeToggle />
             <Separator orientation="vertical" className="h-4" />
             <a href="/auth/logout">
