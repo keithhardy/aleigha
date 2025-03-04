44ef82b02c68ff8925578bd5d94a3fbc1b8e48c8
Keith Hardy <keithjnrhardy@gmail.com>
2025-03-04 22:48:14 +0000

    updated rates

--- app/(dashboard)/certificates/electrical-installation-condition-report/[id]/update/schedule-of-rates/page.tsx
+++ app/(dashboard)/certificates/electrical-installation-condition-report/[id]/update/schedule-of-rates/page.tsx
@@ -1,5 +1,16 @@
+import { Header, HeaderGroup, Heading } from '@/components/page-header';
 import { ScheduleOfRatesForm } from './form'
 
 export default function ScheduleOfRates() {
-  return <ScheduleOfRatesForm />
+  return (
+    <div className="container mx-auto max-w-screen-lg">
+      <Header>
+        <HeaderGroup>
+          <Heading>Schedule Of Rates</Heading>
+        </HeaderGroup>
+      </Header>
+
+      <ScheduleOfRatesForm />
+    </div>
+  );
 }
