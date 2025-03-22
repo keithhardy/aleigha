8efc131feb14f2e45d91784d5a0d019157a7ec46
Keith Hardy <keithjnrhardy@gmail.com>
2025-03-22 23:28:41 +0000

    Update need work

--- app/(dashboard)/certificates/eicr/[id]/update/observations/form.tsx
+++ app/(dashboard)/certificates/eicr/[id]/update/observations/form.tsx
@@ -2,7 +2,7 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ElectricalInstallationConditionReport } from "@prisma/client";
-import { Check, ChevronsUpDown, MoveLeft } from "lucide-react";
+import { Check, ChevronsUpDown, Ellipsis, ExternalLink, MoreVertical, MoveLeft } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
 import { useFieldArray, useForm } from "react-hook-form";
@@ -35,13 +35,18 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { UnsavedChangesDialog } from "@/components/unsaved-changes-dialog";
 import { useToast } from "@/hooks/use-toast";
 
@@ -49,6 +54,8 @@ import { updateObservations } from "./action";
 import { observations } from "./observations";
 import { UpdateObservationsSchema } from "./schema";
 import { sections } from "../components/sections";
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+import { Separator } from "@/components/ui/separator";
 
 export function UpdateObservationsForm({
   certificate,
@@ -79,14 +86,10 @@ export function UpdateObservationsForm({
     });
   };
 
-  const [selectedObservationOpen, setSelectedObservationOpen] = useState(false);
-
   const { fields, append, remove } = useFieldArray({
     control: form.control,
     name: "observations",
   });
-
-  const [selectedObservation, setSelectedObservation] = useState("");
 
   const handleObservationSelect = (value: string) => {
     const observation = observations.find((obs) => obs.id === parseInt(value));
@@ -97,7 +100,6 @@ export function UpdateObservationsForm({
         code: observation.code,
         location: observation.location,
       });
-      setSelectedObservation("");
     }
   };
 
@@ -123,35 +125,27 @@ export function UpdateObservationsForm({
 
           <div className="space-y-4">
             <Card className="rounded-md shadow-none">
-              <div className="flex flex-col gap-4 p-6 lg:flex-row">
+              <div className="flex flex-col items-center gap-4 p-6 lg:flex-row">
                 <CardHeader className="w-full p-0">
                   <CardTitle>Observations</CardTitle>
                   <CardDescription className="text-balance">
-                    Select observations.
+                    Select relevant observations from the predefined list.
                   </CardDescription>
                 </CardHeader>
                 <CardContent className="w-full space-y-4 p-0">
                   <FormItem>
-                    <Popover
-                      open={selectedObservationOpen}
-                      onOpenChange={setSelectedObservationOpen}
-                    >
-                      <PopoverTrigger asChild className="w-full">
+                    <ResponsiveDialog
+                      trigger={
                         <Button
                           variant="outline"
                           role="combobox"
-                          aria-expanded={selectedObservationOpen}
-                          className="flex items-center justify-between"
+                          className="flex items-center justify-between w-full"
                         >
-                          <span>
-                            {selectedObservation
-                              ? `${selectedObservation}: ${observations.find((obs) => obs.id.toString() === selectedObservation)?.description}`
-                              : "Select an observation"}
-                          </span>
+                          Select an observation
                           <ChevronsUpDown className="ml-2 opacity-50" />
                         </Button>
-                      </PopoverTrigger>
-                      <PopoverContent className="min-w-[375px] p-0">
+                      }
+                      content={(setOpen) => (
                         <Command
                           filter={(value, search) => {
                             if (!search) return 1;
@@ -171,100 +165,116 @@ export function UpdateObservationsForm({
                                   key={observation.id}
                                   value={`${observation.itemNumber} ${observation.description}`}
                                   onSelect={() => {
-                                    setSelectedObservation(
-                                      observation.id.toString(),
-                                    );
                                     handleObservationSelect(
                                       observation.id.toString(),
                                     );
-                                    setSelectedObservationOpen(false);
+                                    setOpen(false);
                                   }}
                                 >
                                   {`${observation.itemNumber}: ${observation.description}`}
-                                  {observation.id.toString() ===
-                                  selectedObservation ? (
-                                    <Check className="ml-auto" />
-                                  ) : null}
                                 </CommandItem>
                               ))}
                             </CommandGroup>
                           </CommandList>
                         </Command>
-                      </PopoverContent>
-                    </Popover>
+                      )}
+                    />
                   </FormItem>
-                  {fields.map((field, index) => (
-                    <div key={field.id} className="space-y-2">
-                      <FormField
-                        control={form.control}
-                        name={`observations.${index}.itemNumber`}
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel>Item number</FormLabel>
-                            <FormControl>
-                              <Input placeholder="4.19" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                      <FormField
-                        control={form.control}
-                        name={`observations.${index}.description`}
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel>Description</FormLabel>
-                            <FormControl>
-                              <Textarea
-                                placeholder="SPDs not provided for protection against transient overvoltage."
-                                {...field}
-                                className="h-[100px]"
-                              />
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                      <FormField
-                        control={form.control}
-                        name={`observations.${index}.code`}
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel>Code</FormLabel>
-                            <FormControl>
-                              <Input placeholder="C3" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                      <FormField
-                        control={form.control}
-                        name={`observations.${index}.location`}
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel>Location</FormLabel>
-                            <FormControl>
-                              <Input
-                                placeholder="Electrical cupboard"
-                                {...field}
-                              />
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                      <Button type="button" onClick={() => remove(index)}>
-                        Delete
-                      </Button>
-                    </div>
-                  ))}
                 </CardContent>
               </div>
+              <CardContent>
+                {fields.length > 0 && (
+                  <>
+                    <Card className="hidden md:block rounded-md shadow-none">
+                      <CardContent className="p-0">
+                        <Table className="text-sm">
+                          <TableHeader>
+                            <TableRow className="h-8">
+                              <TableHead className="pl-6">Item Number</TableHead>
+                              <TableHead>Code</TableHead>
+                              <TableHead>Description</TableHead>
+                              <TableHead className="text-right pr-6">Actions</TableHead>
+                            </TableRow>
+                          </TableHeader>
+                          <TableBody>
+                            {fields.map((field, index) => (
+                              <TableRow key={index}>
+                                <TableCell className="pl-6">{field.itemNumber}</TableCell>
+                                <TableCell>{field.code}</TableCell>
+                                <TableCell>{field.description}</TableCell>
+                                <TableCell className="text-right pr-6">
+                                  <DropdownMenu>
+                                    <DropdownMenuTrigger asChild>
+                                      <Button variant="ghost" size="icon">
+                                        <Ellipsis className="h-4 w-4" />
+                                        <span className="sr-only">Open menu</span>
+                                      </Button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent align="end">
+                                      <DropdownMenuItem>Edit</DropdownMenuItem>
+                                      <DropdownMenuItem>Delete</DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                  </DropdownMenu>
+                                </TableCell>
+                              </TableRow>
+                            ))}
+                          </TableBody>
+                        </Table>
+                      </CardContent>
+                    </Card>
+                    <Card className="md:hidden rounded-md shadow-none">
+                      <CardContent className="p-0">
+                        <div>
+                          {fields.map((field, index) => (
+                            <div key={index}>
+                              <div className="flex items-start justify-between p-6">
+                                <div className="space-y-1.5 text-sm w-full">
+                                  <div className="flex items-center justify-between gap-2">
+                                    <div>
+                                      <span>{field.itemNumber}</span>
+                                      <span className="text-muted-foreground"> - </span>
+                                      <span>{field.code}</span>
+                                    </div>
+                                    <DropdownMenu>
+                                      <DropdownMenuTrigger asChild>
+                                        <Button variant="ghost" size="icon" className="-mt-2">
+                                          <Ellipsis className="h-4 w-4" />
+                                          <span className="sr-only">Open menu</span>
+                                        </Button>
+                                      </DropdownMenuTrigger>
+                                      <DropdownMenuContent align="end">
+                                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                                      </DropdownMenuContent>
+                                    </DropdownMenu>
+                                  </div>
+                                  <p>{field.description}</p>
+                                </div>
+                                <div>
+                                </div>
+                              </div>
+                              {index !== fields.length - 1 && <Separator />}
+                            </div>
+                          ))}
+                        </div>
+                      </CardContent>
+
+                    </Card>
+                  </>
+                )}
+
+              </CardContent>
               <CardFooter className="flex justify-between space-x-4 rounded-b-md border-t bg-muted py-4">
                 <p className="text-balance text-sm text-muted-foreground">
-                  Review the details of the observations and add any missing
-                  information.
+                  Can't find an observation? Add one in{" "}
+                  <Link
+                    href={"/properties"}
+                    className="inline-flex items-center space-x-1 text-blue-500"
+                  >
+                    <span>Observations</span>
+                    <ExternalLink size={14} />
+                  </Link>
+                  .
                 </p>
               </CardFooter>
             </Card>
